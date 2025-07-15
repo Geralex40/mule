0e09e645078ee6fdf5089ba4a20863351ece1cb2
German Acuna <germanacuna011@gmail.com>
2025-07-15 10:19:55 -0600

    pipipi

--- webhook/webhook/main.py
+++ webhook/webhook/main.py
@@ -2,7 +2,7 @@ import uvicorn
 import os
 
 host="localhost"
-port = int(os.environ.get("PORT", 8000))
+port = int(os.environ.get("PORT", 5000))
 
 if __name__ == "__main__":
     uvicorn.run("webhook:app", host=host, port=port, reload=True)
