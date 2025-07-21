ba6d2f1772c59ea5388e90eb779ce76e0d95556d
German Acuna <germanacuna011@gmail.com>
2025-07-21 12:32:30 -0600

    post en XML con id consecutivo

--- webhook/webhook/webhook.py
+++ webhook/webhook/webhook.py
@@ -10,10 +10,16 @@ app = FastAPI()
 print(hostin,port)
 miHostPost="http://localhost:"+str(port)+"/webhooksPost"
 miHostGet="http://localhost:"+str(port)+"/webhooksGet"
+miHostXml="http://localhost:"+str(port)+"/webhooksXmlPost"
 print(miHostPost)
 
 class endpoints(BaseModel):
     endPoint: str
+    delay: int
+
+class endpointsXml(BaseModel):
+    endPoint: str
+    idInitial: int
     delay: int
 
 class payload(BaseModel):
@@ -28,6 +34,27 @@ posts = [
         "start_date": "2025-07-14 10:17:00.837074"
     }
 ]
+
+#plantilla xml y su funcion para cambiar sus datos
+data=""
+idInicial=1
+nombreInicial="a"
+def defineXml(idInicial2,nombre):
+    idInicial=idInicial2
+    global data 
+    data = """
+    <?xml version="1.0"?>
+    <shampoo>
+    <id>
+        """+str(idInicial)+"""
+    </id>
+    <Name>
+        """+nombre+"""
+    </Name>
+    </shampoo>
+    """
+    return data
+#print(defineXml(2,"alex"))
 
 @app.get("/format")
 async def get_posts() -> dict:
@@ -45,7 +72,15 @@ async def add_post(posteo: endpoints) -> dict:
     print(posteo.dict())
     requests.post(miHostGet, json=posteo.dict(),timeout=1)
     return {"status": "Triggered webhook"}
+#detonar el webhook que manda XML, indicandole sus parametros
+@app.post("/xml-parameters", tags=["webhook config"])
+async def add_post(posteo: endpointsXml) -> dict:
+    print("hola")
+    print(posteo.dict())
+    requests.post(miHostXml, json=posteo.dict(),timeout=1)
+    return {"status": "Triggered webhook xml"}
 
+#variable bandera para que funcione o no el webhook
 i=0
 #webhook que manda posts
 @app.post("/webhooksPost",tags=["WebHooks"])
@@ -60,14 +95,40 @@ def new_subscription(body: endpoints = Body(...),tags=["posts"]):
     return {"status": "Triggered second endpoint", "response_code": response.status_code}
 #webhook que manda gets
 @app.post("/webhooksGet",tags=["WebHooks"])
-def new_subscription(body: endpoints = Body(...),tags=["posts"]):
+def postToGet(body: endpoints = Body(...),tags=["posts"]):
     print(body)
     global i
     while i<1:
-        requests.get(body.endPoint)
+        requests.get(body.endPoint)#funcion de requests que envia gets, a pesar de estar dentro de un post de FastApi
         print("enviado")
         time.sleep(body.delay) 
     return {"status": "Webhook ended"}
+#webhook que postea xml
+@app.post("/webhooksXmlPost",tags=["WebHooks"])
+def postToGet(body: endpointsXml = Body(...),tags=["posts"]):
+    print(body)
+    global i
+    idActual=body.idInitial
+    while i<1:
+        verPost=requests.post(body.endPoint, data=defineXml(idActual,"alex"))
+        idActual=idActual+1
+        print(verPost)#ejecuta el post
+        time.sleep(body.delay) 
+    return {"status": "Webhook ended"}
+# #gets?
+# @app.get("/legacy/")
+# def get_legacy_data():
+#     data = """<?xml version="1.0"?>
+#     <shampoo>
+#     <Header>
+#         Apply shampoo here.
+#     </Header>
+#     <Body>
+#         You'll have to use soap here.
+#     </Body>
+#     </shampoo>
+#     """
+#     return Response(content=data, media_type="application/xml")
 
 #detener el webhook
 @app.get("/stop",tags=["webHookControl"])
